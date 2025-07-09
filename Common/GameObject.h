4b80aecf38cdb2c401dd6a1bf7a9667672cccc28
Mikhanil <raeinforce@gmail.com>
2025-07-09 22:33:40 +0300

    change BenchmarkState intrerface
    add CameraReset method

--- Common/GameObject.h
+++ Common/GameObject.h
@@ -16,7 +16,7 @@ class Component;
 class Transform;
 class Renderer;
 
-class GameObject
+class GameObject final
 {
 public:
     GameObject();
@@ -27,14 +27,14 @@ public:
 
     void virtual Update(const GameTimer* gt = nullptr);
 
-    void virtual Draw(const std::shared_ptr<GCommandList>& cmdList);
+    void Draw(const std::shared_ptr<GCommandList>& cmdList);
 
     std::shared_ptr<Transform>& GetTransform();
 
     std::shared_ptr<Renderer>& GetRenderer();
 
     template <class T = Component>
-    void AddComponent(std::shared_ptr<T> component)
+    inline void AddComponent(std::shared_ptr<T> component)
     {
         component->gameObject = this;
         components.push_back(component);
@@ -56,7 +56,7 @@ public:
 
     void SetScale(float scale) const;
 
-    void SetScale(Vector3& scale) const;
+    void SetScale(const Vector3& scale) const;
 
     std::string& GetName() { return name; }
 
